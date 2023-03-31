9045f5504047eae69ef169a1ac3b36958e208e47
SayuGita <sayu@undiksha.ac.id>
2023-04-01 00:21:16 +0800

    [Krishna] Update before rules

--- application/models/konsultasi.php
+++ application/models/konsultasi.php
@@ -15,6 +15,7 @@ class konsultasi extends CI_Model
         $gejala = $this->input->post('gejala');
         $namaGejala = array();
         $in = null;
+
         for ($i = 0; $i < count($gejala); $i++) {
             if ($i + 1 == count($gejala)) {
                 $in .= $gejala[$i];
@@ -22,6 +23,7 @@ class konsultasi extends CI_Model
                 $in .= $gejala[$i] . ",";
             }
         }
+
         $query = $this->db->query("SELECT id_gejala, nama_gejala as NamaGejala from tb_gejala WHERE id_gejala in ($in)");
         $result = $query->result();
         return $result;
@@ -41,11 +43,35 @@ class konsultasi extends CI_Model
                 $in .= $gejala[$i] . ",";
             }
         }
+
         $query = $this->db->query("SELECT * from tb_rules WHERE id_gejala in ($in)");
         $result = $query->result();
-        print_r($result);
-        // for ($i = 0; $i < count($gejala); $i++) {
 
-        // }
+        $resValue = json_decode(json_encode($result), true);
+        $i = 0;
+        $lastPenyakit = null;
+        $idPenyakitList = [];
+        $indexEnd = [];
+        foreach ($resValue as $key => $values) {
+            if ($values['id_penyakit'] != $lastPenyakit) {
+                $lastPenyakit = $values['id_penyakit'];
+                if ($i - 1 != -1) array_push($indexEnd, $i - 1);
+                array_push($idPenyakitList, $values['id_penyakit']);
+            }
+            echo "id_penyakit: " . $values['id_penyakit'];
+            echo nl2br("\n");
+            echo "nilai CF: " . $values['nilai_cf'] / 10;
+            echo nl2br("\n");
+            if (isset($usercf[$i])) {
+                echo "nilai_user: " . $usercf[$i];
+            }
+            echo nl2br("\n");
+            $i++;
+            if ($i + 1 == count($resValue)) {
+                array_push($indexEnd, $i);
+            }
+        }
+        print_r($idPenyakitList);
+        print_r($indexEnd);
     }
 }
